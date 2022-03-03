cf10ccf8b6da850f4585530ed2cd01a5ea80fb22
Jerika Kanz <jerikacapila@gmail.com>
2022-03-03 20:42:17 +0100

    Bundling

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development", // or production
+  entry: {
+    main: path.resolve(__dirname, "src/app.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
 };
